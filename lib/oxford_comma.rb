acd4d02e5102adae1dd2baafe0b6882e8b091556
chejianchao <820992376@qq.com>
2018-07-06 06:33:50 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.size == 1
     return array[0]
   else if array.size == 2
-    return array[0]+" "+array[1]
+    return array[0]+" and "+array[1]
   else
     item = array.pop()
     str = array.join(", ")
